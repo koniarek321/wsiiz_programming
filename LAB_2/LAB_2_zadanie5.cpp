9d02943d6cca80856eff38a2474f9d8f29dd2ac6
koniarek321 <92634019+koniarek321@users.noreply.github.com>
2021-11-26 11:36:22 +0100

    Add files via upload

---
+++ LAB_2/LAB_2_zadanie5.cpp
@@ -0,0 +1,24 @@
+﻿// LAB_2_zadanie5.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, suma=0, liczba;
+
+	cout << "wprowadz ilosc liczb ciagu: ", cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Wprowadz liczbe:", cin >> liczba;
+		if (liczba % 2 == 0) {
+			suma += liczba;			
+		}
+		else
+			continue;
+	}
+	cout << suma << endl;
+}
+
---
+++ LAB_2/LAB_2_zadanie6.cpp
@@ -0,0 +1,39 @@
+﻿// LAB_2_zadanie6.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	int n, liczba, suma=0;
+	srand(time(NULL));
+
+	cout << "ile liczb chcesz wylosowac? ", cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		liczba = rand() % 56 - 10;
+		cout << liczba << endl;
+
+		if (liczba % 2 == 0)
+		{
+			suma += liczba;
+		}
+	}
+	cout << "Suma: " << suma << endl;
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
